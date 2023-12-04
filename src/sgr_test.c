42d76cccafcdcc2cd1645b6e498acd2555226767
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-12-04 23:45:52 +0900

    文字の色と背景色をつける

--- src/sgr_test.c
+++ src/sgr_test.c
@@ -12,6 +12,17 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("\033[31m");
+	puts("赤い文字");
+	printf("\033[0m");
+
+	printf("\033[43m");
+	puts("黄色い背景");
+	printf("\033[31;43m");
+	puts("赤い文字黄色い背景");
+
+	printf("\033[0m");
+	puts("元通り");
+
 	return EXIT_SUCCESS;
 }
